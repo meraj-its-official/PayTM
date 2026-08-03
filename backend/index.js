6926b9320fce13381202131fc009fc598e17d5d6
MD MERAJ ALAM <meraj_2312res814@iitp.ac.in>
2026-08-03 21:17:06 +0530

    update for DNS bypass

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+const dns = require('dns')
+dns.setServers(['8.8.8.8', '8.8.4.4'])
 require('dotenv').config(); // Ye line .env file se data utha kar process.env mein daalti hai
 const express = require('express')
 const mainRouter = require('./routes/index.js')
@@ -16,4 +18,5 @@ app.use((err, req, res, next) => {
     res.status(500).send("Sorry ! Invalid Cridentals");
 })
 
-app.listen(PORT, () => { });
+const PORT = process.env.PORT;
+app.listen(PORT);
